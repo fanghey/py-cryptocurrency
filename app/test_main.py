de26971a2b77b9966130a2f02faaf2eeedef766a
fanghey <2603Lysenko2603@gmail.com>
2026-06-02 13:57:22 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,20 @@
 from unittest.mock import patch
 from app.main import cryptocurrency_action
 
+
 @patch("app.main.get_exchange_rate_prediction")
 def test_buy_more(mock_predict: "Mock") -> None:
     mock_predict.return_value = 110
     result = cryptocurrency_action(100)
     assert result == "Buy more cryptocurrency"
 
+
 @patch("app.main.get_exchange_rate_prediction")
 def test_sell_all(mock_predict: "Mock") -> None:
     mock_predict.return_value = 90
     result = cryptocurrency_action(100)
     assert result == "Sell all your cryptocurrency"
+
 
 @patch("app.main.get_exchange_rate_prediction")
 def test_do_nothing(mock_predict: "Mock") -> None:
