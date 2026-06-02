36e2582cb6478c031a947239eb0766c574dcac5e
fanghey <2603Lysenko2603@gmail.com>
2026-06-02 13:52:58 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,8 @@
 from unittest.mock import patch
 from app.main import cryptocurrency_action
-@patch('app.main.get_exchange_rate_prediction')
-def test_buy_more(mock_predict):
-    mock_predict.return_value = 110 
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_buy_more(mock_predict: "Mock") -> None:
+    mock_predict.return_value = 110
     result = cryptocurrency_action(100)
     assert result == "Buy more cryptocurrency"
