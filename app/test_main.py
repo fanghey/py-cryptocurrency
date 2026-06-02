c3c0e8c0be8a28a81649cd534b291d13ea5b32ec
fanghey <2603Lysenko2603@gmail.com>
2026-06-02 13:54:42 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -6,3 +6,15 @@ def test_buy_more(mock_predict: "Mock") -> None:
     mock_predict.return_value = 110
     result = cryptocurrency_action(100)
     assert result == "Buy more cryptocurrency"
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_sell_all(mock_predict: "Mock") -> None:
+    mock_predict.return_value = 90
+    result = cryptocurrency_action(100)
+    assert result == "Sell all your cryptocurrency"
+
+@patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing(mock_predict: "Mock") -> None:
+    mock_predict.return_value = 102
+    result = cryptocurrency_action(100)
+    assert result == "Do nothing"
